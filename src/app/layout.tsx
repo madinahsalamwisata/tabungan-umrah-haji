82e4d24a442d79df1a05bd5521aa86999c84f91c
aryamzidane@gmail.com <aryamzidane@gmail.com>
2026-07-12 10:28:32 +0700

    fix: change Script strategy to afterInteractive to prevent React 19 hydration errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <Script 
           src="https://app.sandbox.midtrans.com/snap/snap.js" 
           data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
         />
       </body>
     </html>
